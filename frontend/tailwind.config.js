e52ad785e64d375d44b5b98f607f22feded694f7
Grey Lee <elohim@gmail.com>
2025-12-13 14:17:50 +0800

    fix: Context menu background

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -24,6 +24,10 @@ export default {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
         accent: {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
