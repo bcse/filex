42e189bd88a0d6ea0bba5fd47c347246aa8e08cc
Grey Lee <elohim@gmail.com>
2025-12-16 02:46:30 +0800

    update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,2 @@
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t filex --load ..
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t ghcr.io/bcse/filex --load ..
+docker push ghcr.io/bcse/filex:latest
