9590926679e97272310f92f62620f06468576daf
Grey Lee <elohim@gmail.com>
2026-01-09 02:05:54 +0800

    init native macos desktop app

---
+++ macos/Filex/FilexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilexApp.swift
+//  Filex
+//
+//  Created by Grey Lee on 2026/1/9.
+//
+
+import SwiftUI
+
+@main
+struct FilexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
