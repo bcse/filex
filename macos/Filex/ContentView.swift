171eed204a86ac6b8884a763e74f28203dcb08d7
Grey Lee <elohim@gmail.com>
2026-01-09 02:05:54 +0800

    init native macos desktop app

---
+++ macos/Filex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filex
+//
+//  Created by Grey Lee on 2026/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos/FilexTests/FilexTests.swift
@@ -0,0 +1,17 @@
+//
+//  FilexTests.swift
+//  FilexTests
+//
+//  Created by Grey Lee on 2026/1/9.
+//
+
+import Testing
+@testable import Filex
+
+struct FilexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
